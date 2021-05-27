aeb57abf18409b41edfd7ae3efff7cfb44492f08
ashea27 <shea.andrew27@gmail.com>
2021-05-27 12:32:27 -0600

    turned off more eslint rules to resolve deploy errors

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -18,6 +18,11 @@ module.exports = {
     "object-curly-spacing": "off",
     "indent": "off",
     "no-unused-vars": "off",
+    "no-trailing-spaces": "off",
+    "max-len": "off",
+    "comma-dangle": "off",
+    "require-jsdoc": "off",
+    "eol-last": "off",
   },
   plugins: [
     "@babel/eslint-plugin",
